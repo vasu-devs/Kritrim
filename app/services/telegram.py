89121304c4894d59e1cf078415210214bf181477
Vasu-DevS <siddhvasudev1402@gmail.com>
2026-03-04 10:42:27 +0000

    feat: Add Telegram voice message download functionality.

--- app/services/telegram.py
+++ app/services/telegram.py
@@ -6,3 +6,14 @@ async def send_message(chat_id: int , text: str):
     async with httpx.AsyncClient() as client:
         response=await client.post(url,json={"chat_id": chat_id, "text":text})
     
+async def download_voice(file_id: str):
+    url=f"https://api.telegram.org/bot{settings.telegram_bot_token}/getFile?file_id={file_id}"
+    async with httpx.AsyncClient() as client:
+        response1 = await client.get(url)
+
+        body=response1.json()
+        file_path=body["result"]["file_path"]
+        
+        response2 = await client.get(f"https://api.telegram.org/file/bot{settings.telegram_bot_token}/{file_path}")
+
+        return response2.content
